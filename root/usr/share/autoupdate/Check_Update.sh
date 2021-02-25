1f546d4c3f641a9c26b18c70c1bdca8a57f9ec3e
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-25 13:58:54 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -7,18 +7,17 @@ if [ ! -f /bin/AutoUpdate.sh ];then
 	echo "未检测到 /bin/AutoUpdate.sh" > /tmp/cloud_version
 	exit
 fi
-CURRENT_V="$(awk 'NR==1' /etc/openwrt_info)"
-CURRENT_DEVICE="$(awk 'NR==3' /etc/openwrt_info)"
-CURRENT_D="$(awk 'NR==6' /etc/openwrt_info)"
+Firmware_opmz="$(awk 'NR==5' /etc/openwrt_info)"
+Firmware_zuozhe="$(awk 'NR==6' /etc/openwrt_info)"
 [[ -z "${CURRENT_DEVICE}" ]] && CURRENT_DEVICE="$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')"
 Github="$(awk 'NR==2' /etc/openwrt_info)"
 [[ -z "${Github}" ]] && exit
 Author="${Github##*com/}"
-Github_Tags="https://api.github.com/repos/${Author}/releases/tags/update_Firmware"
+Github_Tags="https://api.github.com/repos/${Author}/releases/latest"
 wget -q ${Github_Tags} -O - > /tmp/Github_Tags
 Firmware_Type="$(awk 'NR==4' /etc/openwrt_info)"
 case ${CURRENT_DEVICE} in
-x86_64)
+x86-64)
 	if [ -d /sys/firmware/efi ];then
 		Firmware_SFX="-UEFI.${Firmware_Type}"
 		BOOT_Type="-UEFI"
@@ -32,8 +31,7 @@ x86_64)
 	BOOT_Type=""
 ;;
 esac
-
-Cloud_Version="$(cat /tmp/Github_Tags | egrep -o "AutoBuild-${CURRENT_D}-[a-z]+.[0-9]+.[0-9]+.[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-z]+.[0-9]+.[0-9]+.[0-9]+')"
+Cloud_Version="$(cat /tmp/Github_Tags | egrep -o "${Firmware_opmz}-${Firmware_zuozhe}-[a-z]+.[0-9]+.[0-9]+.[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-z]+.[0-9]+.[0-9]+.[0-9]+')"
 CURRENT_Version="$(awk 'NR==1' /etc/openwrt_info)"
 if [[ ! -z "${Cloud_Version}" ]];then
 	if [[ "${CURRENT_Version}" == "${Cloud_Version}" ]];then
@@ -41,7 +39,7 @@ if [[ ! -z "${Cloud_Version}" ]];then
 	else
 		Checked_Type="可更新"
 	fi
-	echo "${Cloud_Version} [${Checked_Type}]" > /tmp/cloud_version
+	echo "${Firmware_zuozhe}-${Cloud_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version
 else
 	echo "未知" > /tmp/cloud_version
 fi
