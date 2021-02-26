d2542afff32a4eb9edc5b89e00cde2c27634a6ef
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-26 13:38:44 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -31,7 +31,7 @@ x86-64)
 	BOOT_Type=""
 ;;
 esac
-Cloud_Ver="$(cat /tmp/Github_Tags | egrep -o "${Firmware_opmz}-[a-z]+.[a-z]+.[a-z]+.[a-z]+.[0-9]+.[0-9]+.[0-9]+.[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-z]+.[a-z]+.[a-z]+.[a-z]+.[0-9]+.[0-9]+.[0-9]+.[0-9]+')"
+Cloud_Ver="$(cat /tmp/Github_Tags | egrep -o "${Firmware_opmz}-[a-zA-Z0-9_-]+.[0-9]+.[0-9]+.[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-zA-Z0-9_-]+.[0-9]+.[0-9]+.[0-9]+')"
 Cloud_Version="${Cloud_Ver#*${Firmware_opmz}-}"
 CURRENT_Version="$(awk 'NR==1' /etc/openwrt_info)"
 if [[ ! -z "${Cloud_Version}" ]];then
