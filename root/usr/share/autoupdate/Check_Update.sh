2cd00377f4b8c71bdb5925c4a61e5106b4596097
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-27 13:54:34 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -8,7 +8,8 @@ if [ ! -f /bin/AutoUpdate.sh ];then
 	exit
 fi
 CURRENT_DEVICE="$(awk 'NR==3' /etc/openwrt_info)"
-Firmware_opmz="$(awk 'NR==5' /etc/openwrt_info)"
+Firmware_COMP1="$(awk 'NR==5' /etc/openwrt_info)"
+Firmware_COMP2="$(awk 'NR==6' /etc/openwrt_info)"
 [[ -z "${CURRENT_DEVICE}" ]] && CURRENT_DEVICE="$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')"
 Github="$(awk 'NR==2' /etc/openwrt_info)"
 [[ -z "${Github}" ]] && exit
@@ -31,8 +32,8 @@ x86-64)
 	BOOT_Type=""
 ;;
 esac
-Cloud_Ver="$(cat /tmp/Github_Tags | egrep -o "${Firmware_opmz}-[a-zA-Z0-9_-]+.[0-9]+.[0-9]+.[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-zA-Z0-9_-]+.[0-9]+.[0-9]+.[0-9]+')"
-Cloud_Version="${Cloud_Ver#*${Firmware_opmz}-}"
+Cloud_Ver="$(cat /tmp/Github_Tags | egrep -o "${Firmware_COMP1}-${Firmware_COMP2}+.[a-zA-Z0-9_-]+.[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-zA-Z0-9_-]+.[0-9]+')"
+Cloud_Version="${Cloud_Ver#*${Firmware_COMP1}-}"
 CURRENT_Version="$(awk 'NR==1' /etc/openwrt_info)"
 if [[ ! -z "${Cloud_Version}" ]];then
 	if [[ "${CURRENT_Version}" == "${Cloud_Version}" ]];then
